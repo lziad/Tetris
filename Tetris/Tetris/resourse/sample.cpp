e44f31e2300398ff7592e9fc610c13833626b33b
tsssp <173043733@qq.com>
2017-04-27 20:21:43 +0800

    debug

--- Tetris/Tetris/resourse/sample.cpp
+++ Tetris/Tetris/resourse/sample.cpp
@@ -1,10 +1,13 @@
-﻿/*
+/**
+ * Tetris JSON交互样例程序
  * https://wiki.botzone.org/index.php?title=Tetris
- * 注意：x的范围是1~MAPWIDTH，y的范围是1~MAPHEIGHT
- * 数组是先行（y）后列（c）
- * 坐标系：原点在左下角
- * 平台保证所有输入都是合法输入
+ * 更新于2017年4月20日：
+ * 修正了rotation函数、将交互方式修改为新规则的格式，还有transfer函数里`if (h2 >= MAPHEIGHT)`改为`if (h2 > MAPHEIGHT)`
  */
+// 注意：x的范围是1~MAPWIDTH，y的范围是1~MAPHEIGHT
+// 数组是先行（y）后列（c）
+// 坐标系：原点在左下角
+ 
 #include <iostream>
 #include <string>
 #include <cmath>
@@ -12,46 +15,37 @@
 #include <cstdlib>
 #include <ctime>
 #include "jsoncpp/json.h"
-
-#ifndef _BOTZONE_ONLINE
-#include "LocalTestPackage.h"
-#else
+using namespace std;
+ 
 #define MAPWIDTH 10
 #define MAPHEIGHT 20
-#endif
-
-using namespace std;
-
-//TODO modify max scoreS
-#define INF 999999999
-
-
+ 
 // 我所在队伍的颜色（0为红，1为蓝，仅表示队伍，不分先后）
-int myColor;
+int currBotColor;
 int enemyColor;
-
-/* 先y后x，记录地图状态，0为空，1为以前放置，2为刚刚放置，负数为越界	  */
-/* （2用于在清行后将最后一步撤销再送给对方）						  */
+ 
+// 先y后x，记录地图状态，0为空，1为以前放置，2为刚刚放置，负数为越界
+// （2用于在清行后将最后一步撤销再送给对方）
 int gridInfo[2][MAPHEIGHT + 2][MAPWIDTH + 2] = { 0 };
-
+ 
 // 代表分别向对方转移的行
 int trans[2][4][MAPWIDTH + 2] = { 0 };
-
+ 
 // 转移行数
 int transCount[2] = { 0 };
-
+ 
 // 运行eliminate后的当前高度
 int maxHeight[2] = { 0 };
-
+ 
 // 总消去行数的分数之和
 int elimTotal[2] = { 0 };
-
+ 
 // 一次性消去行数对应分数
 const int elimBonus[] = { 0, 1, 3, 5, 7 };
-
+ 
 // 给对应玩家的各类块的数目总计
-int typeCount[2][7] = { 0 };
-
+int typeCountForColor[2][7] = { 0 };
+ 
 const int blockShape[7][4][8] = {
 	{ { 0,0,1,0,-1,0,-1,-1 },{ 0,0,0,1,0,-1,1,-1 },{ 0,0,-1,0,1,0,1,1 },{ 0,0,0,-1,0,1,-1,1 } },
 	{ { 0,0,-1,0,1,0,1,-1 },{ 0,0,0,-1,0,1,1,1 },{ 0,0,1,0,-1,0,-1,1 },{ 0,0,0,1,0,-1,-1,-1 } },
@@ -61,87 +55,39 @@ const int blockShape[7][4][8] = {
 	{ { 0,0,0,-1,0,1,0,2 },{ 0,0,1,0,-1,0,-2,0 },{ 0,0,0,1,0,-1,0,-2 },{ 0,0,-1,0,1,0,2,0 } },
 	{ { 0,0,0,1,-1,0,-1,1 },{ 0,0,-1,0,0,-1,-1,-1 },{ 0,0,0,-1,1,-0,1,-1 },{ 0,0,1,0,0,1,1,1 } }
 };// 7种形状(长L| 短L| 反z| 正z| T| 直一| 田格)，4种朝向(上左下右)，8:每相邻的两个分别为x，y
-
-
-/* 0     botzone.org			*/
-/* 1     simulate botzone.org	*/
-/* 2     Host local game		*/
-int MODE;
-
-/****************************************************************************************************/
-/******************************                                        ******************************/
-/******************************        functions and classes           ******************************/
-/******************************                                        ******************************/
-/****************************************************************************************************/
-
-// 旋转中心的x轴坐标
-// 旋转中心的y轴坐标
-// 标记方块的朝向 0~3
-struct Block
-{
-	int x, y, o;
-
-	Block() {}
-	Block(const int &x, const int &y, const int &o)
-		:x(x), y(y), o(o) {}
-
-	Block(const Json::Value& jv)
-	{
-		x = jv["x"].asInt();
-		y = jv["y"].asInt();
-		o = jv["o"].asInt();
-	}
-
-	operator Json::Value()const
-	{
-		Json::Value jv;
-		jv["x"] = x;
-		jv["y"] = y;
-		jv["o"] = o;
-		return jv;
-	}
-};
-
+ 
+ 
 class Tetris
 {
 public:
 	const int blockType;   // 标记方块类型的序号 0~6
-	Block block;
+	int blockX;            // 旋转中心的x轴坐标
+	int blockY;            // 旋转中心的y轴坐标
+	int orientation;       // 标记方块的朝向 0~3
 	const int(*shape)[8]; // 当前类型方块的形状定义
-
+ 
 	int color;
-
-	Tetris(int t, int color) : blockType(t), shape(blockShape[t]), color(color) {}
-
-
+ 
+	Tetris(int t, int color) : blockType(t), shape(blockShape[t]), color(color)
+	{ }
+ 
 	inline Tetris &set(int x = -1, int y = -1, int o = -1)
 	{
-
-		block.x = x == -1 ? block.x : x;
-		block.y = y == -1 ? block.y : y;
-		block.o = o == -1 ? block.o : o;
+		blockX = x == -1 ? blockX : x;
+		blockY = y == -1 ? blockY : y;
+		orientation = o == -1 ? orientation : o;
 		return *this;
 	}
-
-	inline Tetris &set(const Block& _block = { -1,-1,-1 })
-	{
-
-		block.x = _block.x == -1 ? block.x : _block.x;
-		block.y = _block.y == -1 ? block.y : _block.y;
-		block.o = _block.o == -1 ? block.o : _block.o;
-		return *this;
-	}
-
+ 
 	// 判断当前位置是否合法
 	inline bool isValid(int x = -1, int y = -1, int o = -1)
 	{
-
-		x = x == -1 ? block.x : x;
-		y = y == -1 ? block.y : y;
-		o = o == -1 ? block.o : o;
+		x = x == -1 ? blockX : x;
+		y = y == -1 ? blockY : y;
+		o = o == -1 ? orientation : o;
 		if (o < 0 || o > 3)
 			return false;
-
+ 
 		int i, tmpX, tmpY;
 		for (i = 0; i < 4; i++)
 		{
@@ -154,57 +100,57 @@ public:
 		}
 		return true;
 	}
-
+ 
 	// 判断是否落地
 	inline bool onGround()
 	{
-		if (isValid() && !isValid(-1, block.y - 1))
+		if (isValid() && !isValid(-1, blockY - 1))
 			return true;
 		return false;
 	}
-
+ 
 	// 将方块放置在场地上
 	inline bool place()
 	{
 		if (!onGround())
 			return false;
-
+ 
 		int i, tmpX, tmpY;
 		for (i = 0; i < 4; i++)
 		{
-			tmpX = block.x + shape[block.o][2 * i];
-			tmpY = block.y + shape[block.o][2 * i + 1];
+			tmpX = blockX + shape[orientation][2 * i];
+			tmpY = blockY + shape[orientation][2 * i + 1];
 			gridInfo[color][tmpY][tmpX] = 2;
 		}
 		return true;
 	}
-
+ 
 	// 检查能否逆时针旋转自己到o
 	inline bool rotation(int o)
 	{
 		if (o < 0 || o > 3)
 			return false;
-
-		if (block.o == o)
+ 
+		if (orientation == o)
 			return true;
-
-		int fromO = block.o;
+ 
+		int fromO = orientation;
 		while (true)
 		{
 			if (!isValid(-1, -1, fromO))
 				return false;
-
+ 
 			if (fromO == o)
 				break;
-
+ 
 			fromO = (fromO + 1) % 4;
 		}
 		return true;
 	}
 };
-
+ 
 // 围一圈护城河
-void stateInit()
+void init()
 {
 	int i;
 	for (i = 0; i < MAPHEIGHT + 2; i++)
@@ -218,9 +164,10 @@ void stateInit()
 		gridInfo[0][0][i] = gridInfo[0][MAPHEIGHT + 1][i] = -2;
 	}
 }
-
+ 
 namespace Util
 {
+ 
 	// 检查能否从场地顶端直接落到当前位置
 	inline bool checkDirectDropTo(int color, int blockType, int x, int y, int o)
 	{
@@ -236,7 +183,7 @@ namespace Util
 			}
 		return true;
 	}
-
+ 
 	// 消去行
 	void eliminate(int color)
 	{
@@ -281,7 +228,7 @@ namespace Util
 		maxHeight[color] -= count;
 		elimTotal[color] += elimBonus[count];
 	}
-
+ 
 	// 转移双方消去的行，返回-1表示继续，否则返回输者
 	int transfer()
 	{
@@ -297,11 +244,11 @@ namespace Util
 			if (h2 > MAPHEIGHT)
 				return color2;
 			int i, j;
-
+ 
 			for (i = h2; i > transCount[color1]; i--)
 				for (j = 1; j <= MAPWIDTH; j++)
 					gridInfo[color2][i][j] = gridInfo[color2][i - transCount[color1]][j];
-
+ 
 			for (i = transCount[color1]; i > 0; i--)
 				for (j = 1; j <= MAPWIDTH; j++)
 					gridInfo[color2][i][j] = trans[color1][i - 1][j];
@@ -312,31 +259,31 @@ namespace Util
 			int h1, h2;
 			maxHeight[color1] = h1 = maxHeight[color1] + transCount[color2];//从color1处移动count1去color2
 			maxHeight[color2] = h2 = maxHeight[color2] + transCount[color1];
-
+ 
 			if (h1 > MAPHEIGHT) return color1;
 			if (h2 > MAPHEIGHT) return color2;
-
+ 
 			int i, j;
 			for (i = h2; i > transCount[color1]; i--)
 				for (j = 1; j <= MAPWIDTH; j++)
 					gridInfo[color2][i][j] = gridInfo[color2][i - transCount[color1]][j];
-
+ 
 			for (i = transCount[color1]; i > 0; i--)
 				for (j = 1; j <= MAPWIDTH; j++)
 					gridInfo[color2][i][j] = trans[color1][i - 1][j];
-
+ 
 			for (i = h1; i > transCount[color2]; i--)
 				for (j = 1; j <= MAPWIDTH; j++)
 					gridInfo[color1][i][j] = gridInfo[color1][i - transCount[color2]][j];
-
+ 
 			for (i = transCount[color2]; i > 0; i--)
 				for (j = 1; j <= MAPWIDTH; j++)
 					gridInfo[color1][i][j] = trans[color2][i - 1][j];
-
+ 
 			return -1;
 		}
 	}
-
+ 
 	// 颜色方还能否继续游戏
 	inline bool canPut(int color, int blockType)
 	{
@@ -351,13 +298,19 @@ namespace Util
 				}
 		return false;
 	}
-
+ 
 	// 打印场地用于调试
 	inline void printField()
 	{
-		static const char *i2s[] = { "~~","~~","  ","[]","##" };
-
-		cout << endl;
+#ifndef _BOTZONE_ONLINE
+		static const char *i2s[] = {
+			"~~",
+			"~~",
+			"  ",
+			"[]",
+			"##"
+		};
+		cout << "~~：墙，[]：块，##：新块" << endl;
 		for (int y = MAPHEIGHT + 1; y >= 0; y--)
 		{
 			for (int x = 0; x <= MAPWIDTH + 1; x++)
@@ -366,223 +319,154 @@ namespace Util
 				cout << i2s[gridInfo[1][y][x] + 2];
 			cout << endl;
 		}
-		cout << endl;
-		cout << "（图例： ~~：墙，[]：块，##：新块）" << endl << endl;
-	}
-}
-
-//both required in botzone.org and localTest
-int programInit()
-{
-	istream::sync_with_stdio(false);		// 加速输入
-	srand((unsigned)time(nullptr));
-	stateInit();
-#ifdef __APPLE__
-	freopen("/Users/whitephosphorus/Desktop/in.txt", "r", stdin);
 #endif
-
-#ifdef _BOTZONE_ONLINE
-	MODE = 0;
-#else
-	cout << "Local: Please enter test mode: ";
-	cin >> MODE;
-	if (cin.fail())
-		exit(0);
-#endif // _BOTZONE_ONLINE
-
-	return 0;
-}
-
-//read between { and }. return false when failure
-bool getJsonStr(istream& in, Json::Value& json)
-{
-	string str;
-	Json::Reader reader;
-
-	int stk = 1;
-	char beginChar;
-	in >> beginChar;
-	if (beginChar != '{')
-		return false;
-	str = "{";
-	int ch;
-	while ((ch = cin.get()) != EOF)
-	{
-		str.push_back(char(ch));
-		if (ch == '{')stk++;
-		if (ch == '}')stk--;
-		if (stk == 0)
-			break;
 	}
-	if (stk != 0)
-		return false;
-
-
-	return reader.parse(str, json);
 }
-
-int recoverState(int(&nextType)[2])
+ 
+ 
+int main()
 {
-	//读入JSON
+	// 加速输入
+	istream::sync_with_stdio(false);
+	srand(time(NULL));
+	init();
+ 
+	int turnID, blockType;
+	int nextTypeForColor[2];
+ 
+	// 读入JSON
+	string str;
+	getline(cin, str);
+	Json::Reader reader;
 	Json::Value input;
-	getJsonStr(cin, input);
-#ifndef _BOTZONE_ONLINE
-	interpretSeverLog(input);
-#endif // !_BOTZONE_ONLINE
-
-	int curTurnID, tmpBlockType;
-
-	/* 先读入第一回合，得到自己的颜色	*/
-	/* 双方的第一块肯定是一样的		*/
-	curTurnID = input["responses"].size() + 1;
+	reader.parse(str, input);
+ 
+	// 先读入第一回合，得到自己的颜色
+	// 双方的第一块肯定是一样的
+	turnID = input["responses"].size() + 1;
 	auto &first = input["requests"][(Json::UInt) 0];
-
-	tmpBlockType = first["block"].asInt();
-	myColor = first["color"].asInt();
-
-	enemyColor = 1 - myColor;
-	nextType[0] = tmpBlockType;
-	nextType[1] = tmpBlockType;
-	typeCount[0][tmpBlockType]++;
-	typeCount[1][tmpBlockType]++;
-
-
-	/* 然后分析以前每回合的输入输出，并恢复状态			*/
-	/* 循环中，color 表示当前这一行是 color 的行为		*/
-
-	for (int i = 1; i < curTurnID; i++)
+ 
+	blockType = first["block"].asInt();
+	currBotColor = first["color"].asInt();
+ 
+	enemyColor = 1 - currBotColor;
+	nextTypeForColor[0] = blockType;
+	nextTypeForColor[1] = blockType;
+	typeCountForColor[0][blockType]++;
+	typeCountForColor[1][blockType]++;
+ 
+ 
+	// 然后分析以前每回合的输入输出，并恢复状态
+	// 循环中，color 表示当前这一行是 color 的行为
+	// 平台保证所有输入都是合法输入
+	for (int i = 1; i < turnID; i++)
 	{
-		/* 根据这些输入输出逐渐恢复状态到当前回合					*/
-		int currType[2] = { nextType[0], nextType[1] };
-		Block block;
-
-		/* 先读自己的输出，也就是自己的行为						*/
-		/* 然后模拟放置块									 		*/
+		int currTypeForColor[2] = { nextTypeForColor[0], nextTypeForColor[1] };
+		int x, y, o;
+		// 根据这些输入输出逐渐恢复状态到当前回合
+ 
+		// 先读自己的输出，也就是自己的行为
+		// 自己的输出是一个序列，但是只有最后一步有用
+		// 所以只保留最后一步
+		// 然后模拟最后一步放置块
 		auto &myOutput = input["responses"][i - 1];
-		tmpBlockType = myOutput["block"].asInt();
-		block = myOutput;
-
+		blockType = myOutput["block"].asInt();
+		x = myOutput["x"].asInt();
+		y = myOutput["y"].asInt();
+		o = myOutput["o"].asInt();
+ 
 		// 我当时把上一块落到了 x y o！
-		Tetris myBlock(currType[myColor], myColor);
-		myBlock.set(block).place();
-
+		Tetris myBlock(currTypeForColor[currBotColor], currBotColor);
+		myBlock.set(x, y, o).place();
+ 
 		// 我给对方什么块来着？
-		typeCount[enemyColor][tmpBlockType]++;
-		nextType[enemyColor] = tmpBlockType;
-
-		/* 然后读对方的输入，也就是对方的行为   */
-		/* 裁判给自己的输入只有对方的最后一步   */
+		typeCountForColor[enemyColor][blockType]++;
+		nextTypeForColor[enemyColor] = blockType;
+ 
+		// 然后读自己的输入，也就是对方的行为
+		// 裁判给自己的输入是对方的最后一步
 		auto &myInput = input["requests"][i];
-		tmpBlockType = myInput["block"].asInt();
-		block = myInput;
-
+		blockType = myInput["block"].asInt();
+		x = myInput["x"].asInt();
+		y = myInput["y"].asInt();
+		o = myInput["o"].asInt();
+ 
 		// 对方当时把上一块落到了 x y o！
-		Tetris enemyBlock(currType[enemyColor], enemyColor);
-		enemyBlock.set(block).place();
-
+		Tetris enemyBlock(currTypeForColor[enemyColor], enemyColor);
+		enemyBlock.set(x, y, o).place();
+ 
 		// 对方给我什么块来着？
-		typeCount[myColor][tmpBlockType]++;
-		nextType[myColor] = tmpBlockType;
-
+		typeCountForColor[currBotColor][blockType]++;
+		nextTypeForColor[currBotColor] = blockType;
+ 
 		// 检查消去
 		Util::eliminate(0);
 		Util::eliminate(1);
-
+ 
 		// 进行转移
 		Util::transfer();
 	}
-	return 0;
-}
-
-//TODO: localize varible
-int blockForEnemy;
-Block result;
-
-//role: host(0)/guest(1)
-int NegativeMaxSearch(
-	const int(&gridInfo)[2][MAPHEIGHT + 2][MAPWIDTH + 2],
-	const int nextBlockType, int depth, int alpha, int beta, int role)
-{
-	/* 贪心决策												*/
-	/* 从下往上以各种姿态找到第一个位置，要求能够直着落下		*/
-	Tetris block(nextBlockType, myColor);
+ 
+ 
+	int blockForEnemy, finalX, finalY, finalO;
+ 
+	// 做出决策（你只需修改以下部分）
+ 
+	// 遇事不决先输出（平台上运行不会输出）
+	Util::printField();
+ 
+	// 贪心决策
+	// 从下往上以各种姿态找到第一个位置，要求能够直着落下
+	Tetris block(nextTypeForColor[currBotColor], currBotColor);
 	for (int y = 1; y <= MAPHEIGHT; y++)
 		for (int x = 1; x <= MAPWIDTH; x++)
 			for (int o = 0; o < 4; o++)
 			{
 				if (block.set(x, y, o).isValid() &&
-					Util::checkDirectDropTo(myColor, block.blockType, x, y, o))
+					Util::checkDirectDropTo(currBotColor, block.blockType, x, y, o))
 				{
-					result = { x,y,o };
+					finalX = x;
+					finalY = y;
+					finalO = o;
 					goto determined;
 				}
 			}
-
+ 
 determined:
 	// 再看看给对方什么好
-
+ 
 	int maxCount = 0, minCount = 99;
 	for (int i = 0; i < 7; i++)
 	{
-		if (typeCount[enemyColor][i] > maxCount)
-			maxCount = typeCount[enemyColor][i];
-		if (typeCount[enemyColor][i] < minCount)
-			minCount = typeCount[enemyColor][i];
+		if (typeCountForColor[enemyColor][i] > maxCount)
+			maxCount = typeCountForColor[enemyColor][i];
+		if (typeCountForColor[enemyColor][i] < minCount)
+			minCount = typeCountForColor[enemyColor][i];
 	}
 	if (maxCount - minCount == 2)
 	{
 		// 危险，找一个不是最大的块给对方吧
 		for (blockForEnemy = 0; blockForEnemy < 7; blockForEnemy++)
-			if (typeCount[enemyColor][blockForEnemy] != maxCount)
+			if (typeCountForColor[enemyColor][blockForEnemy] != maxCount)
 				break;
 	}
 	else
 	{
 		blockForEnemy = rand() % 7;
 	}
-
-
-	return 0;
-}
-
-void outputResult(const int &blockForEnemy, const Block &result)
-{
+ 
+	// 决策结束，输出结果（你只需修改以上部分）
+ 
 	Json::Value output;
 	Json::FastWriter writer;
-
-	output["response"] = result;
+ 
 	output["response"]["block"] = blockForEnemy;
-	
-
+ 
+	output["response"]["x"] = finalX;
+	output["response"]["y"] = finalY;
+	output["response"]["o"] = finalO;
+ 
 	cout << writer.write(output);
-}
-
-int main()
-{
-	programInit();
-
-	if (MODE == 0 || MODE == 1)
-	{
-		int nextType[2];
-
-		recoverState(nextType);
-
-#ifndef _BOTZONE_ONLINE
-		Util::printField();
-#endif // _BOTZONE_ONLINE
-
-		NegativeMaxSearch(gridInfo, nextType[myColor], 1, -INF, INF, 0);
-
-		outputResult(blockForEnemy, result);
-	}
-#ifndef _BOTZONE_ONLINE
-	else if (MODE == 2)
-	{
-		gameEngineWork();
-	}
-#endif // _BOTZONE_ONLINE
-	else
-		exit(0);
-	
+ 
 	return 0;
 }
